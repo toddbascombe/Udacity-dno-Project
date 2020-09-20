38ecee524fc6f9fa2134cc2c70f9306620b38052
tech@ccnyalumni.org <tech@ccnyalumni.org>
2020-09-20 00:06:52 -0400

    Add random fact generator

--- app.js
+++ app.js
@@ -1,83 +1,39 @@
 //Revealing module pattern
-//In the closure the weightCompare, diet, and heigthcompare function are private 
-let Dino = (function(){
-  let dinos = null
-  let human = null
-  newDinoData = null;
+//In the closure the weightCompare, diet, and heigthcompare function are private
+class Dino {
+  constructor({ species, weight, height, diet, where, when, fact }) {
+    this.species = species;
+    this.weight = weight;
+    this.height = height;
+    this.diet = diet;
+    this.where = where;
+    this.when = when;
+    this.fact = fact;
+    this.image = "images/" + this.species + ".png";
+  }
+
   //Compare the dino weight to human weight
-  function weightCompare() {
-    newDinoData = dinos.map((dino) => {
-      if (dino.weight > parseInt(human.weight, 10)) {
-        dino.weight = `weight: ${
-          dino.weight - parseInt(human.weight, 10)
-        } lbs more`;
-      } else if (dino.weight === parseInt(human.weight, 10)) {
-        dino.weight = `same weight: ${dino.weight}`;
-      } else {
-        dino.weight = `weigh: ${
-          parseInt(human.weight, 10) - dino.weight
-        }lbs less`;
-      }
-      return dino;
-    });
+  weightCompare(humanData) {
+    return `Weight: ${this.weight} lb, your weight is ${humanData} lb`;
   }
   //helper function to convert height into inches
-  function feetToInches(ft) {
+  feetToInches(ft) {
     return ft * 12.0;
   }
 
-    //compare human height(inches) to Dino height
-  function heightCompare() {
-    newDinoData = dinos.map((dino) => {
-      let heightInInches = parseInt(feetToInches(human.feet), 10);
-      if (dino.height > heightInInches) {
-        dino.height = `Height: ${dino.height - heightInInches} inches more`;
-      } else if (dino.height === heightInInches) {
-        dino.height = `same Height: ${dino.height}`;
-      } else {
-        dino.height = `Height: ${heightInInches - dino.height} inches less`;
-      }
-      return dino;
-    });
+  //compare human height(inches) to Dino height
+  heightCompare(humanData) {
+    const height_inches = this.feetToInches(parseInt(humanData));
+    return `Height: ${this.height} inches and your height is ${height_inches} inches`;
   }
 
   //Method compare the human diet and the dino diet
-  function dietCompare() {
-    newDinoData = dinos.map((dino) => {
-      if (dino.diet === human.diet.toLowerCase()) {
-        dino.diet = `Same Diet: ${dino.diet}`;
-      } else {
-        dino.diet = `Diet: ${dino.diet}`;
-      }
-      return dino;
-    });
+  dietCompare(humanData) {
+    return `Diet: ${this.diet} and your diet is ${humanData}`;
   }
+}
 
-  //get dino data compare to the human
-  function dinoCompare() {
-    weightCompare();
-    heightCompare();
-    dietCompare();
-    return newDinoData;
-  }
-
-  //set data to the variable
-  function setData(dinoData, humanData){
-    dinos = dinoData
-    human = humanData
-  }
-
-
-  return {
-    DinoCompare: dinoCompare,
-    SetData: setData 
-  }
-
-})()
-
-
-
-const get_Dino_data = {};
+const get_Dino_data = [];
 const Human = {};
 
 //setup elements to create for the human tiles
@@ -92,25 +48,25 @@ const humanDataElementToCreate = [
   {
     elementName: "p",
     attributes: {
-      className:['data__disappear'],
+      className: ["data__disappear"],
       id: ["p_data_1"],
     },
   },
   {
     elementName: "p",
     attributes: {
-      className:['data__disappear'],
+      className: ["data__disappear"],
       id: ["p_data_2"],
     },
   },
   {
     elementName: "p",
     attributes: {
-      className:['data__disappear'],
+      className: ["data__disappear"],
       id: ["p_data_3"],
     },
   },
-]
+];
 
 //setup elements to create for the other tiles
 const elementsToCreate = [
@@ -124,54 +80,58 @@ const elementsToCreate = [
   {
     elementName: "img",
     attributes: {
-      className:['data__disappear'],
-    }
+      className: ["data__disappear"],
+    },
   },
   {
     elementName: "p",
     attributes: {
-      className:['data__disappear'],
+      className: ["data__disappear"],
       id: ["p_data_1"],
-    },
-  },
-  {
-    elementName: "p",
-    attributes: {
-      className:['data__disappear'],
-      id: ["p_data_2"],
-    },
-  },
-  {
-    elementName: "p",
-    attributes: {
-      className:['data__disappear'],
-      id: ["p_data_3"],
-    },
-  },
-  {
-    elementName: "p",
-    attributes: {
-      className:['data__disappear'],
-      id: ["p_data_4"],
     },
   },
 ];
 
-
-
 //when page loads up fetch dino data
 (async function () {
-  await fetch("dino.json")
-    .then(async (data) => {
-      const newData = await data.json();
-      get_Dino_data["data"] = newData.Dinos;
-    })
+  await fetch("dino.json").then(async (data) => {
+    const newData = await data.json();
+    get_Dino_data.push(
+      ...newData.Dinos.map((dino) => {
+        return new Dino({ ...dino });
+      })
+    );
+  });
 })();
-
 
 //needs to be random
 const randomNumber = (endNumber) => {
   return Math.floor(Math.random() * Math.floor(endNumber));
+};
+
+const random_fact = (dino, randomNum) => {
+  let fact = "";
+  switch (randomNum) {
+    case 1:
+      fact = dino.dietCompare(Human.diet);
+      break;
+    case 2:
+      fact = dino.weightCompare(Human.weight);
+      break;
+    case 3:
+      fact = dino.heightCompare(Human.feet);
+      break;
+    case 4:
+      fact = `The ${dino.species} was found in the ${dino.when}.`;
+      break;
+    case 5:
+      fact = `The ${dino.species} lived in what is now ${dino.where}.`;
+      break;
+    default:
+      fact = dino.fact;
+      break;
+  }
+  return fact;
 };
 
 //find a number in a given array
@@ -179,7 +139,7 @@ const numberFinder = (arr, number) => {
   return arr.includes(number);
 };
 
-//given a array of elements, add the centertile to the center of the elements 
+//given a array of elements, add the centertile to the center of the elements
 const placingTileCenter = (elements, centerTile) => {
   let centerLocation = Math.floor((elements.length + 1) / 2);
   elements.splice(centerLocation, 0, centerTile);
@@ -192,46 +152,41 @@ const elementCreator = (elementToCreate, attribute) => {
   if (attribute !== undefined && attribute.className) {
     element.classList.add(...attribute.className);
   }
-  if (attribute !== undefined && !(attribute.id == null) ) {
+  if (attribute !== undefined && !(attribute.id == null)) {
     element.id = attribute.id[0];
   }
   return element;
 };
 
-// add a child element to a given parent element 
+// add a child element to a given parent element
 const addElementsToParent = (parentElement, childrenElement) => {
-  parentElement.appendChild(childrenElement)
+  parentElement.appendChild(childrenElement);
 };
 
 //add dino and human data to a array of elements
-const elementData = (elements, data, type) => {
-  if(type === 'dino'){
+const elementData = (elements, data, type, randomNum) => {
+  if (type === "dino") {
     return elements.map((element) => {
-      switch (true){
-        case(element.tagName.toLowerCase() === 'h3'):
+      switch (true) {
+        case element.tagName.toLowerCase() === "h3":
           return (element.textContent = data.species);
-        case(element.tagName.toLowerCase() === 'p' && element.id === 'p_data_1'):
-          return element.textContent = data.weight
-        case(element.tagName.toLowerCase() === 'p' && element.id === 'p_data_2'):
-          return (element.textContent = data.height);
-        case(element.tagName.toLowerCase() === 'p' && element.id === 'p_data_3'):
-          return (element.textContent = data.diet);
-        case(element.tagName.toLowerCase() === 'p' && element.id === 'p_data_4'):
-          return (element.textContent = data.fact);
-        case(element.tagName.toLowerCase() === 'img'):
+        case element.tagName.toLowerCase() === "p":
+          const fact = random_fact(data, randomNum);
+          return (element.textContent = fact);
+        case element.tagName.toLowerCase() === "img":
           return (element.src = `./images/${data.species.toLowerCase()}.png`);
       }
-    })
-  }else{
+    });
+  } else {
     return elements.map((element) => {
-      switch(true){
-        case(element.tagName.toLowerCase() === 'h3'):
+      switch (true) {
+        case element.tagName.toLowerCase() === "h3":
           return (element.textContent = data.name);
-        case(element.tagName.toLowerCase() === 'p' && element.id === 'p_data_1'):
-          return element.textContent = data.weight
-        case(element.tagName.toLowerCase() === 'p' && element.id === 'p_data_2'):
+        case element.tagName.toLowerCase() === "p" && element.id === "p_data_1":
+          return (element.textContent = data.weight);
+        case element.tagName.toLowerCase() === "p" && element.id === "p_data_2":
           return (element.textContent = data.feet);
-        case(element.tagName.toLowerCase() === 'p' && element.id === 'p_data_3'):
+        case element.tagName.toLowerCase() === "p" && element.id === "p_data_3":
           return (element.textContent = data.diet);
       }
     });
@@ -239,30 +194,27 @@ const elementData = (elements, data, type) => {
 };
 
 //append all elements to given parentElement
-const findingAllElementsToAppend = (parent, elements)=>{
-  if(parent === 'grid-item'){
-    for(let i = 0; i < elements.length; i++){
-      if(i >=  1){
-        addElementsToParent(elements[0], elements[i])
+const findingAllElementsToAppend = (parent, elements) => {
+  if (parent === "grid-item") {
+    for (let i = 0; i < elements.length; i++) {
+      if (i >= 1) {
+        addElementsToParent(elements[0], elements[i]);
       }
     }
   }
-}
-
+};
 
 //generate random tiles
 const tiles_generate = () => {
   const elements = [];
   const randomNumberSelected = [];
-  Dino.SetData(get_Dino_data.data, Human)
-  const newDino = Dino.DinoCompare()
-
-  while (randomNumberSelected.length < get_Dino_data["data"].length) {
-    let number = randomNumber(get_Dino_data["data"].length);
+  let factRandomNumber = randomNumber(7);
+  while (randomNumberSelected.length < get_Dino_data.length) {
+    let number = randomNumber(get_Dino_data.length);
     if (!numberFinder(randomNumberSelected, number)) {
       randomNumberSelected.push(number);
     } else {
-      number = randomNumber(get_Dino_data["data"].length);
+      number = randomNumber(get_Dino_data.length);
     }
   }
 
@@ -274,13 +226,18 @@ const tiles_generate = () => {
       );
     });
 
-    elementData(elementsToDisplay, newDino[number], 'dino');
+    elementData(
+      elementsToDisplay,
+      get_Dino_data[number],
+      "dino",
+      factRandomNumber
+    );
     //main parent append Children
-    findingAllElementsToAppend('grid-item', elementsToDisplay)
-    elements.push(elementsToDisplay[0]); 
+    findingAllElementsToAppend("grid-item", elementsToDisplay);
+    elements.push(elementsToDisplay[0]);
   }
 
-  const elementsToDisplay = humanDataElementToCreate.map(elementToCreate => {
+  const elementsToDisplay = humanDataElementToCreate.map((elementToCreate) => {
     return elementCreator(
       elementToCreate.elementName,
       elementToCreate.attributes
@@ -289,7 +246,7 @@ const tiles_generate = () => {
 
   elementData(elementsToDisplay, Human);
   //main parent append Children
-  findingAllElementsToAppend('grid-item', elementsToDisplay)
+  findingAllElementsToAppend("grid-item", elementsToDisplay);
 
   return placingTileCenter(elements, elementsToDisplay[0]);
 };
@@ -308,19 +265,21 @@ const removeForm = () => {
 };
 
 //when mouse is over the tile display dino and human data
-const onMouseOver = (event)=>{
-  if (event.target.className === 'grid-item'){
-    let grid_item = event.target
-    for(let child = 0; child < grid_item.children.length; child++){
-      if(grid_item.children[child].className == undefined || grid_item.children[child].className !== 'data__disappear'){
-        grid_item.children[child].classList.add('data__disappear')
-      }else{
-        grid_item.children[child].classList.remove('data__disappear')
+const onMouseOver = (event) => {
+  if (event.target.className === "grid-item") {
+    let grid_item = event.target;
+    for (let child = 0; child < grid_item.children.length; child++) {
+      if (
+        grid_item.children[child].className == undefined ||
+        grid_item.children[child].className !== "data__disappear"
+      ) {
+        grid_item.children[child].classList.add("data__disappear");
+      } else {
+        grid_item.children[child].classList.remove("data__disappear");
       }
     }
-  }  
-}
-
+  }
+};
 
 // On button click, prepare and display infographic
 const btn = document.querySelector("#btn");
@@ -336,11 +295,9 @@ btn.addEventListener("click", () => {
     Human.weight = weight.value;
     Human.diet = diet.value;
     addTilesToDOM();
-  })().then(()=>{
+  })().then(() => {
     //when submit button fuction is finish lissten for a mouseover on a tile
-    const grid = document.querySelector('#grid')
-    grid.addEventListener('mouseover', onMouseOver, false)
-    
-  })
-})
-
+    const grid = document.querySelector("#grid");
+    grid.addEventListener("mouseover", onMouseOver, false);
+  });
+});
